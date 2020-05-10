34648a8a2d7a713a893058e4638f94c81f539250
edwinjosegeorge <52248484+EJG2000@users.noreply.github.com>
2020-05-10 16:38:24 +0530

    assignment 4

---
+++ assignments/Assignment4/SpeakGoodBye.js
@@ -0,0 +1,6 @@
+(function (){
+  var byeSpeaker = new Object();
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) { console.log(speakWord + " " + name); }
+  window.byeSpeaker = byeSpeaker;
+})();
